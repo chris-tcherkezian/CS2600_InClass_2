c2cd50f37584d182057d78be397956743fa8d742
Chris Tcherkezian <ctcherkezian@cpp.edu>
2021-10-06 21:41:13 -0700

    main code

---
+++ employee.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stddef.h>
+
+typedef struct {
+    long number;
+    char *name;
+    char *phone;
+    long salary;
+} Employee, *PtrToEmployee;
+
+typedef const Employee *PtrToConstEmployee;
